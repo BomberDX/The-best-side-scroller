3cbb57055f7d43c330ea423df5e85b485d3b463f
Tomhsu1 <TomHsu@missionbits-MacBook-Air-59.local>
2016-03-16 18:03:17 -0700

    Game screen #2

--- directory.js
+++ directory.js
@@ -4,8 +4,6 @@ var game = new Phaser.Game(725, 450, Phaser.AUTO, 'gameDiv');
 //adds the TitleScreen as a Phaser state to the game
 game.state.add('TitleScreen', TitleScreen);
 
-game.state.add('GameScreen', GameScreen);
-
 //To add more states "game.state.add('what the state will be named, the object that the state is contained within);"
 
 game.state.start('TitleScreen');
