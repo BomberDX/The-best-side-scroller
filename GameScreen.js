db3ec9aca6255aefdf0740b189586de83ee80f7f
Ruellia Zhang <syncyes@gmail.com>
2016-03-22 14:09:05 -0700

    manual merge

--- GameScreen.js
+++ GameScreen.js
@@ -2,12 +2,22 @@ var GameScreen = {
     preload: function() {
         game.load.image('brick', 'assets/images/start1.png');
         game.load.image('ball', 'assets/images/start1.png');
+        game.load.spritesheet('rm', '/assets/images/runningman.png', 64, 64, 10);
     },
     create: function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.cursor = game.input.keyboard.createCursorKeys();
         this.bricks = game.add.group();
         this.bricks.enableBody = true;
+        
+        this.zxc = game.add.sprite(40, 10, 'rm');
+
+        this.zxc.animations.add('walk');
+
+        this.zxc.animations.play('walk', 10, true);
+        game.physics.arcade.enable(this.zxc);
+        this.zxc.body.immovable = true;
+        this.zxc.body.collideWorldBounds = true;
         
         for (var i = 0; i <5; i++) {
             for (var j = 0; j < 5; j++) {
@@ -30,6 +40,22 @@ var GameScreen = {
     },
     update: function() {
         game.physics.arcade.collide(this.ball, this.bricks, this.hit, null, this);
+            this.zxc.body.velocity.x = 0;
+            this.zxc.body.velocity.y = 0;
+        if (this.cursor.right.isDown) {
+            this.zxc.body.velocity.x = 350;
+            this.zxc.anchor.setTo(.5,1);
+            this.zxc.scale.x = -1;
+        } if (this.cursor.left.isDown) { //if the left arrow is pressed, move to the left
+            this.zxc.anchor.setTo(.5,1);
+            this.zxc.scale.x = 1;
+            this.zxc.body.velocity.x = -350;
+        } if (this.cursor.up.isDown){ //if the up arrow is pressed, move upwards
+            this.zxc.body.velocity.y = -350;
+        } if (this.cursor.down.isDown) { //if the down arrow is pressed, move downwards
+            this.zxc.body.velocity.y = 350;
+        }
+
     },
     
     hit: function (ball, brick) {
