07e57b4ad2d356c5c121aa0ea446b64e13ab4bb0
matthew7205 <masilva2@s.sfusd.edu>
2016-03-23 17:14:21 -0700

    Merge pull request #1 from ruellia/master
    manual merge

--- TitleScreen.js
+++ TitleScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //creates a TitleScreen object
 var TitleScreen = {
     
@@ -34,4 +35,21 @@ var TitleScreen = {
         this.state.start('GameScreen');
     }
     
+=======
+var TitleScreen = {
+    
+    // the preload method runs first
+    //it is were we load our assets
+    preload: function(){
+        game.load.image('logo', 'logo.jpg' );
+    },
+    create: function() {
+            this.add.image(490, 300, 'logo');
+            this.add.button(200, 180, 'logo', this.startGame, this);
+            game.state.backgroundColor = '#CCCFFF';
+        },
+    startGame: function() {
+        this.state.start('GameScreen')
+    }
+>>>>>>> origin/Sebastiaan
 };
