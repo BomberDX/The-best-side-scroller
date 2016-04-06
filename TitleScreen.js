f73b0bcc491a2b2bb52cc355f426c9afb8de191c
matthew7205 <masilva2@s.sfusd.edu>
2016-04-06 16:44:08 -0700

    cool

--- TitleScreen.js
+++ TitleScreen.js
@@ -5,27 +5,45 @@ var TitleScreen = {
     //it is where we load our assets
     preload : function() {
         //loads an image named 'logo'
-        game.load.image('zombie', '/assets/images/zombie1.png');
+        game.load.image('chr', '/assets/images/walk_animation.gif');
         //loads an image named 'start'   
         game.load.image('start1', '/assets/images/start1.png');
+        game.load.image('bg' , '/assets/images/game_background.png' );
     },
     
     
     //the create method is run after the preload method
     //it is where we set up the basics of the game, essentially what it will look like when we start the game
     create: function () {
+        this.bg = game.add.image(0,0, 'bg');
+        this.bg.width = game.world.width;
+        this.bg.height = game.world.height;
         //adds an image with image 'logo' at (290, 100)
-        this.zombie = this.add.image( 550, 100, 'zombie');
-        this.zombie.scale.x = 0.5;
-        this.zombie.scale.y = 0.5;
+        this.chr = this.add.image(800, 279, 'chr');
+        this.chr.width = 70;
+        this.chr.height = 40;
+        this.chr.scale.x = 0.5;
+        this.chr.scale.y = 0.5;
         //adds a button with image 'start' at location (200, 180) that calls the method startGame when it is clicked on
-        this.add.button(game.world.centerX-50, game.world.centerY-50, 'start1', this.startGame, this);
+        this.test = this.add.button(1100, game.world.centerY+50, 'start1', this.startGame, this);
         var style = {font: '80px Arial', fill:'#FFFFFF', align: 'center'};
-        game.add.text(game.world.centerX - 50, game.world.centerY, 'Start');
+        this.text = game.add.text(1100, game.world.centerY+100, 'Start');
+    
         
         
         //makes the background color of the whole screen periwinkle
-        game.stage.backgroundColor = '#006600';
+    },
+    
+    update: function() {
+        if (this.test.x > 500) 
+            this.test.x -= 5;
+       if (this.text.x > 500) {
+           this.text.x -= 5;
+        if (this.chr.x > 500) {
+            this.chr.x -= 20
+        }
+            
+       }
     
     },
 
